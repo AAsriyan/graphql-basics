de106eab087f251c6abc2dacd464b10908028333
Arshak Asriyan <aasriyan01@gmail.com>
2019-08-24 20:37:49 -0400

    Added subscriptions

---
+++ src/resolvers/index.resolvers.js
@@ -0,0 +1,15 @@
+const Query = require('./Query');
+const Mutation = require('./Mutation');
+const User = require('./User');
+const Post = require('./Post');
+const Comment = require('./Comment');
+const Subscription = require('./Subscription');
+
+module.exports = {
+  Query,
+  Mutation,
+  User,
+  Post,
+  Comment,
+  Subscription
+}
